0f76515b5f0b891e8fe31a12bba696567d209c7e
unknown <rauboluk@gmail.com>
2023-01-18 20:59:23 +0100

    feat: navbarLink props types fixed

--- src/components/NavbarLink.tsx
+++ src/components/NavbarLink.tsx
@@ -1,28 +1,32 @@
-import type { LinkProps } from 'next/link'
-import type { ReactElement } from 'react'
-import NextLink from 'next/link'
+import type { LinkProps } from '@chakra-ui/react'
 import { Box, Link } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import type { ReactElement } from 'react'
 
 interface Props extends LinkProps {
     active?: boolean
     children: ReactElement
 }
 
-export const NavbarLink = ({ children, ...props }: Props) => {
-
-    const background = props.active ? 'orange.400' : 'transparent'
+export const NavbarLink = ({ children, active, ...props }: Props) => {
+    const background = active ? 'orange.400' : 'transparent'
 
     return (
         <Link
             as={NextLink}
-            href='/'
             w={'100%'}
             h={'60px'}
             display={'flex'}
             justifyContent={'space-between'}
             alignItems={'center'}
+            {...props}
         >
-            <Box w="6px" h={'100%'} background={background} borderRadius={'0 7px 7px 0'}></Box>
+            <Box
+                w="6px"
+                h={'100%'}
+                background={background}
+                borderRadius={'0 7px 7px 0'}
+            ></Box>
             {children}
             <Box w="6px"></Box>
         </Link>
--- src/components/NavbarList.tsx
+++ src/components/NavbarList.tsx
@@ -2,17 +2,22 @@ import { Icon } from './Icon'
 import { NavbarLink } from './NavbarLink'
 
 const elements = [
-    { src: '/menu_icon.svg', alt: 'items', active: false },
-    { src: '/refresh_icon.svg', alt: 'history', active: true },
-    { src: '/analytics_icon.svg', alt: 'statistics', active: false },
+    { href: '/items', src: '/menu_icon.svg', alt: 'items', active: false },
+    { href: '/history', src: '/refresh_icon.svg', alt: 'history', active: true },
+    {
+        href: '/statistics',
+        src: '/analytics_icon.svg',
+        alt: 'statistics',
+        active: false,
+    },
 ]
 
 export const NavbarList = () => {
     return (
         <>
-            {elements.map((element, idx) => (
-                <NavbarLink href="/" active={element.active}>
-                    <Icon key={idx} {...element} />
+            {elements.map((element) => (
+                <NavbarLink key={element.alt} href={element.href} active={element.active}>
+                    <Icon {...element} />
                 </NavbarLink>
             ))}
         </>
