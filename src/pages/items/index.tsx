c171ded5836e5e61d5a29836eac21f995e8faf6b
unknown <rauboluk@gmail.com>
2023-01-23 23:49:33 +0100

    feat: product card

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,11 +1,16 @@
-import ShoppingList from "@/components/ShoppingList"
-import { Box, GridItem } from "@chakra-ui/react"
+import { ProductCard } from '@/components/ProductCard'
+import ShoppingList from '@/components/ShoppingList'
+import { Flex, GridItem } from '@chakra-ui/react'
 
 const index = () => {
     return (
         <>
-            <GridItem as={'section'} >
+            <GridItem as={'section'} p={'2'}>
                 qeqwe
+                <Flex>
+                    <ProductCard name="Test" />
+                    <ProductCard name="Test" />
+                </Flex>
             </GridItem>
             <ShoppingList />
         </>
