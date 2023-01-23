954c1338fd74a7635b25a94202b5d6650cf03076
KonanDestylator <rzepka994@gmail.com>
2023-01-23 19:39:52 +0100

    feat: responsive template for items page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,9 +6,16 @@ function Layout({ children }: { children: ReactElement }) {
     return (
         <Grid templateColumns={'65px 1fr'} minH={'100vh'}>
             <Sidebar />
-            <GridItem>
-                <main>{children}</main>
-            </GridItem>
+            <Grid
+                templateColumns={{
+                    sm: '1fr',
+                    md: '1fr 350px',
+                    xl: '1fr 400px',
+                }}
+                as={'main'}
+            >
+                {children}
+            </Grid>
         </Grid>
     )
 }
--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,5 +1,16 @@
+import { Box, GridItem } from "@chakra-ui/react"
+
 const index = () => {
-    return <div>items</div>
+    return (
+        <>
+            <GridItem as={'section'} >
+                qeqwe
+            </GridItem>
+            <GridItem as={'aside'} background={'#FFF0DE'} height={'100vh'}>
+                qwe
+            </GridItem>
+        </>
+    )
 }
 
 export default index
