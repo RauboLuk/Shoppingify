2c10014256a7af09937f678da90c2ce69d7fd28c
unknown <rauboluk@gmail.com>
2023-01-24 23:07:25 +0100

    feat: row gap

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -8,7 +8,11 @@ const ProductsList = () => {
         { name: 'Pre-cooked corn 450g' },
     ]
     return (
-        <Flex gap={{ base: 2, md: 5 }} wrap={'wrap'}>
+        <Flex
+            gap={{ base: 2, md: 5 }}
+            wrap={'wrap'}
+            rowGap={{ base: 6, md: 7 }}
+        >
             {productsExample.map((product) => (
                 <ProductCard key={product.name} name={product.name} />
             ))}
