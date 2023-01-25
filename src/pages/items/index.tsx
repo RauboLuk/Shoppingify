c2ace839bb6c356199cb937dd2078b0d2a52418e
unknown <rauboluk@gmail.com>
2023-01-25 22:09:42 +0100

    feat: heading text

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,11 +1,25 @@
 import ProductsList from '@/components/ProductsList'
 import ShoppingList from '@/components/ShoppingList'
-import { GridItem } from '@chakra-ui/react'
+import { GridItem, Heading, Highlight } from '@chakra-ui/react'
 
 const index = () => {
     return (
         <>
             <GridItem as={'section'} p={'2'}>
+                <Heading
+                    as="h1"
+                    size={'lg'}
+                    pb={{ base: 8, md: 12 }}
+                    maxW={'30rem'}
+                >
+                    <Highlight
+                        query="Shoppingify"
+                        styles={{ color: 'orange.400' }}
+                    >
+                        Shoppingify
+                    </Highlight>{' '}
+                    allows you take your shopping list wherever you go
+                </Heading>
                 <ProductsList />
             </GridItem>
             <ShoppingList />
