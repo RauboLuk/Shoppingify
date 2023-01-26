859449137a6320c9f58a01de007a6cf44ba2506a
unknown <rauboluk@gmail.com>
2023-01-26 22:05:01 +0100

    fix: import font weights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import Layout from '@/components/Layout'
 import theme from '@/styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
+import '@fontsource/quicksand/300.css'
+import '@fontsource/quicksand/400.css'
+import '@fontsource/quicksand/500.css'
+import '@fontsource/quicksand/600.css'
+import '@fontsource/quicksand/700.css'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 
