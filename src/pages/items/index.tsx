2645b311602ca4c7d5ca997715780db687f100ca
RauboLuk <rauboluk@gmail.com>
2023-01-28 14:07:55 +0100

    feat: items page header (#7)
    * feat: heading text
    * feat: header component, search input

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,11 +1,13 @@
+import Header from '@/components/Header'
 import ProductCategory from '@/components/ProductCategory'
 import ShoppingList from '@/components/ShoppingList'
-import { GridItem, Heading, Highlight } from '@chakra-ui/react'
+import { GridItem } from '@chakra-ui/react'
 
 const index = () => {
     return (
         <>
-            <GridItem as={'section'} p={'2'}>
+            <GridItem as="section" p="2">
+                <Header />
                 <ProductCategory />
             </GridItem>
             <ShoppingList />
